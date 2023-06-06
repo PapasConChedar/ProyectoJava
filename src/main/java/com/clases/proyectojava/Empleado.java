7c5a25481407ff6ef60bcc223049fc3b27c82116
Nicolás  Vercellone <85401681+biltox@users.noreply.github.com>
2023-06-06 12:57:25 -0300

    Creacion de Clase Persona y Empleado
    Clase Persona (abstracta)
    Clase Empleado (extends de Persona)
    
    Ambas clases tienen sus respectivos Getters and Setters, Constructor vacio y ToString.

---
+++ src/main/java/com/clases/proyectojava/Empleado.java
@@ -0,0 +1,51 @@
+package com.clases.proyectojava;
+
+public class Empleado extends Persona{
+private String idEmpleado;
+private String email;
+private String contraseña;
+
+
+// region CONSTRUCTOR VACIO
+    public Empleado(String persona, String apellido, String dni, String direccion) {
+        super(persona, apellido, dni, direccion);
+    }
+    // endregion
+
+    //region GETTERS AND SETTERS
+    public String getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(String idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+// endregion
+
+    //region TOSTRING
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "idEmpleado='" + idEmpleado + '\'' +
+                ", email='" + email + '\'' +
+                ", contraseña='" + contraseña + '\'' +
+                '}';
+    }
+    //endregion
+}
---
+++ src/main/java/com/clases/proyectojava/Persona.java
@@ -0,0 +1,64 @@
+package com.clases.proyectojava;
+
+public class Persona {
+
+    private String persona;
+    private String apellido;
+    private String dni;
+    private String direccion;
+
+//region CONSTRUCTOR VACIO
+    public Persona(String persona, String apellido, String dni, String direccion) {
+        this.persona = persona;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.direccion = direccion;
+    }
+// endregion
+
+    //region GETTERS AND SETTERS
+    public String getPersona() {
+        return persona;
+    }
+
+    public void setPersona(String persona) {
+        this.persona = persona;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+// endregion
+
+    //region TOSTRING
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "persona='" + persona + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", dni='" + dni + '\'' +
+                ", direccion='" + direccion + '\'' +
+                '}';
+    }
+    //endregion
+}
