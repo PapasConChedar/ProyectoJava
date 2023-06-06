b16eb6c0915d28921e54d8f30014bcfcfc71cdc3
Nicolás  Vercellone <85401681+biltox@users.noreply.github.com>
2023-06-06 14:38:25 -0300

    Creacion de Clase Pedido
    Se creo la clase Pedido con su constructor vacio y getters and setters.

---
+++ src/main/java/com/clases/proyectojava/Pedido.java
@@ -0,0 +1,50 @@
+package com.clases.proyectojava;
+
+import java.util.ArrayList;
+
+public class Pedido extends Productos{
+    private int numPedido;
+    private int precio;
+    enum EstadoPago {PAGO, IMPAGO, PENDIENTE}
+    private ArrayList<Productos> productos;
+
+//region CONSTRUCTOR VACIO
+    public Pedido(String marca, String nombre, String fecheaDeVencimiento, String fechaElavoracion, int stock, int precio, int numPedido, int precio1, ArrayList<Productos> productos) {
+        super(marca, nombre, fecheaDeVencimiento, fechaElavoracion, stock, precio);
+        this.numPedido = numPedido;
+        this.precio = precio1;
+        this.productos = productos;
+    }
+    //endregion
+
+    //region GETTERS AND SETTERS
+
+    public int getNumPedido() {
+        return numPedido;
+    }
+
+    public void setNumPedido(int numPedido) {
+        this.numPedido = numPedido;
+    }
+
+    @Override
+    public int getPrecio() {
+        return precio;
+    }
+
+    @Override
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public ArrayList<Productos> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(ArrayList<Productos> productos) {
+        this.productos = productos;
+    }
+
+    //endregion
+    
+}
