d0376d9c73496c23c232ede8d0c2e3676ae7e8a3
ianpatrani <iannpatrnai@gmail.com>
2023-06-17 21:24:14 -0300

    chore: identation

--- src/main/java/com/org/example/service/Utils.java
+++ src/main/java/com/org/example/service/Utils.java
@@ -7,12 +7,13 @@ public class Utils {
     private static final String PATTERN =
             "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +
                     "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-    public static Boolean validEmail(String email){
+
+    public static Boolean validEmail(String email) {
         Matcher matcher = Pattern.compile(PATTERN).matcher(email);
         return matcher.matches();
     }
 
-    public static Boolean validPassword(String password){
+    public static Boolean validPassword(String password) {
         return password.length() >= 8 && password.length() <= 16 && password.matches(".*[A-Z].*") && password.matches(".*[a-z].*") && password.matches(".*[0-9].*");
     }
 
