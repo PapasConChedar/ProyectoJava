c1e7bf07e411d5c4e9eca80cdc30565df19b02e7
Florencia Aguiar <florenciamelanyaguiar@gmail.com>
2023-06-19 02:22:08 -0300

    feat:ilustracion Menu Principal

--- src/main/java/org/example/ventanas/vistas/VistaPrincipal.java
+++ src/main/java/org/example/ventanas/vistas/VistaPrincipal.java
@@ -27,36 +27,53 @@ public class VistaPrincipal extends javax.swing.JPanel {
     private void initComponents() {
 
         backgroundPrincipal = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
 
         setMaximumSize(new java.awt.Dimension(800, 560));
         setMinimumSize(new java.awt.Dimension(800, 560));
         setPreferredSize(new java.awt.Dimension(800, 560));
 
-        backgroundPrincipal.setBackground(new java.awt.Color(255, 51, 0));
+        backgroundPrincipal.setBackground(new java.awt.Color(68, 187, 235));
         backgroundPrincipal.setMaximumSize(new java.awt.Dimension(800, 560));
         backgroundPrincipal.setMinimumSize(new java.awt.Dimension(800, 560));
         backgroundPrincipal.setPreferredSize(new java.awt.Dimension(800, 560));
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
-        jLabel1.setText("VENTANA PRINCIPAL");
+        jPanel1.setBackground(new java.awt.Color(130, 210, 240));
+        jPanel1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(213, 236, 249), 6, true));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 660, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 11, Short.MAX_VALUE)
+        );
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/shoppin1.png"))); // NOI18N
 
         javax.swing.GroupLayout backgroundPrincipalLayout = new javax.swing.GroupLayout(backgroundPrincipal);
         backgroundPrincipal.setLayout(backgroundPrincipalLayout);
         backgroundPrincipalLayout.setHorizontalGroup(
             backgroundPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(backgroundPrincipalLayout.createSequentialGroup()
-                .addGap(266, 266, 266)
-                .addComponent(jLabel1)
-                .addContainerGap(310, Short.MAX_VALUE))
+                .addGap(52, 52, 52)
+                .addGroup(backgroundPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 633, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(76, Short.MAX_VALUE))
         );
         backgroundPrincipalLayout.setVerticalGroup(
             backgroundPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(backgroundPrincipalLayout.createSequentialGroup()
-                .addGap(202, 202, 202)
-                .addComponent(jLabel1)
-                .addContainerGap(326, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, backgroundPrincipalLayout.createSequentialGroup()
+                .addGap(34, 34, 34)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 328, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(169, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -77,5 +94,6 @@ public class VistaPrincipal extends javax.swing.JPanel {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel backgroundPrincipal;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
